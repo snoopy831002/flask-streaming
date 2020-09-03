5c0347195dc2c59d7d3c86a39e86f1cb8d7dff83
snoopy831002 <snoopy831002@gmail.com>
2020-09-04 00:56:22 +0800

    Fix file working dir
    Run script by ./generate_hls.sh .

--- hls-gen/generate_hls.sh
+++ hls-gen/generate_hls.sh
@@ -6,13 +6,14 @@ mkdir -p $1
 # Get into the folder
 cd $1
 
+
 # Start running FFmpeg HLS Live streaming
-python ${PWD}/hls-generator.py | ffmpeg  \
+python hls-generator.py | ffmpeg  \
   -f rawvideo \
   -pixel_format bgr24 \
   -framerate 10 \
   -video_size 1280x720 \
-  -i - foo.mp4  \
+  -i - ../flask-view/video/foo.mp4  \
   -vcodec libx264 \
   -acodec copy \
   -pix_fmt yuv420p \
@@ -21,5 +22,6 @@ python ${PWD}/hls-generator.py | ffmpeg  \
   -hls_list_size 5 \
   -hls_flags delete_segments \
   -use_localtime 1 \
-  -hls_segment_filename '%Y%m%d-%s.ts' \
-  ./playlist.m3u8
+  -hls_segment_filename '../flask-view/video/%Y%m%d-%s.ts' \
+  ../flask-view/video/playlist.m3u8
+  #${PWD}/flask-streaming/flask-view/video/playlist.m3u8
